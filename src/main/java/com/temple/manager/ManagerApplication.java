6f86006bf94f9efcf4387bd70370bb3f2bf5dfd0
ryukiyoun <ryukiyoun@naver.com>
2022-03-13 18:29:35 +0900

    feat: properties 파일 명령 옵션으로 처리
    - 기존 Application Class에 정적 Properties 위치 제거

--- src/main/java/com/temple/manager/ManagerApplication.java
+++ src/main/java/com/temple/manager/ManagerApplication.java
@@ -1,18 +1,12 @@
 package com.temple.manager;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class ManagerApplication {
-	public static final String APPLICATION_PROPERTIES = "spring.config.location="
-			+ "classpath:application.yml,"
-			+ "/home/ec2-user/app/config/prod-application.yml";
-
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(ManagerApplication.class)
-				.properties(APPLICATION_PROPERTIES)
-				.run(args);
+		SpringApplication.run(ManagerApplication.class, args);
 	}
 
 }
