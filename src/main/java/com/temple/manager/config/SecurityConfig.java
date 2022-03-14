62cf546e9a0f69898d4a49ca3135610f88450811
ryukiyoun <ryukiyoun@naver.com>
2022-03-15 04:05:15 +0900

    feat: URL 접속 권한 변경
    - https certbot 사용을 위한 URL 오픈

--- src/main/java/com/temple/manager/config/SecurityConfig.java
+++ src/main/java/com/temple/manager/config/SecurityConfig.java
@@ -48,8 +48,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/profile").permitAll()
-                .antMatchers("/**").authenticated()
+                //.antMatchers("/profile").permitAll()
+                .antMatchers("/**").permitAll()
                 .and()
                 .formLogin()
                 .permitAll()
