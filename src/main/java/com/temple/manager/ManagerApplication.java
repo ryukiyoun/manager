bfac397f2e4da5756bc8f5a071798c5d3d734cbb
ryukiyoun <ryukiyoun@naver.com>
2022-03-16 03:59:57 +0900

    feat: Redis Config 변경
    - EC2 Redis append 오류 수정

--- src/main/java/com/temple/manager/ManagerApplication.java
+++ src/main/java/com/temple/manager/ManagerApplication.java
@@ -2,8 +2,10 @@ package com.temple.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+@EnableRedisHttpSession
 public class ManagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ManagerApplication.class, args);
