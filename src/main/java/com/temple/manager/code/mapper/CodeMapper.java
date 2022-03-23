2e5a8913c58c844562a0b9502a808fc54abbe0ee
ryukiyoun <ryukiyoun@naver.com>
2022-03-23 16:56:14 +0900

    feat: Code Mapper Impl 오류
    - MapStruct Impl 생성 오류 수정

--- src/main/java/com/temple/manager/code/mapper/CodeMapper.java
+++ src/main/java/com/temple/manager/code/mapper/CodeMapper.java
@@ -3,12 +3,11 @@ package com.temple.manager.code.mapper;
 import com.temple.manager.code.dto.CodeDTO;
 import com.temple.manager.code.entity.Code;
 import org.mapstruct.Mapper;
-import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
-@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper
 public interface CodeMapper {
     CodeMapper INSTANCE = Mappers.getMapper(CodeMapper.class);
 
