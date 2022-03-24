6d046fea8e94401142396a28799e967aa3a7a17c
ryukiyoun <ryukiyoun@naver.com>
2022-03-25 00:49:33 +0900

    feat: FamilyMapper Bean 등록
    - 정적 Mapper Instance를 통해 접근하는 것이 아닌 Mapper를 Bean으로 등록하여 Bean으로 접근 하도록 변경
    
    - Mapper Bean 변경으로 인한 Family Test 변경

---
+++ src/test/java/com/temple/manager/mapper/FamilyMapperTest.java
@@ -0,0 +1,221 @@
+package com.temple.manager.mapper;
+
+import com.temple.manager.believer.dto.BelieverDTO;
+import com.temple.manager.believer.entity.Believer;
+import com.temple.manager.config.TestMapperConfig;
+import com.temple.manager.enumable.FamilyType;
+import com.temple.manager.enumable.LunarSolarType;
+import com.temple.manager.family.dto.FamilyDTO;
+import com.temple.manager.family.entity.Family;
+import com.temple.manager.family.mapper.FamilyMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+
+@ExtendWith({MockitoExtension.class, SpringExtension.class})
+@ContextConfiguration(classes = {TestMapperConfig.class})
+public class FamilyMapperTest {
+    @Autowired
+    FamilyMapper familyMapper;
+
+    Family fixture;
+
+    FamilyDTO fixtureDTO;
+
+    List<Family> fixtureList;
+
+    List<FamilyDTO> fixtureDTOList;
+
+    @BeforeEach
+    void init(){
+        fixture = Family.builder()
+                .familyId(1)
+                .familyName("tester1")
+                .familyType(FamilyType.FATHER)
+                .believer(Believer.builder()
+                        .believerId(1)
+                        .believerName("believer1")
+                        .birthOfYear("111111")
+                        .lunarSolarType(LunarSolarType.LUNAR)
+                        .address("부산")
+                        .build())
+                .lunarSolarType(LunarSolarType.LUNAR)
+                .birthOfYear("111111")
+                .etcValue("etc1")
+                .build();
+
+        fixtureList = new ArrayList<>();
+        fixtureList.add(fixture);
+
+        fixtureDTO = FamilyDTO.builder()
+                .familyId(1)
+                .familyName("tester1")
+                .familyType(FamilyType.FATHER)
+                .believer(BelieverDTO.builder()
+                        .believerId(1)
+                        .believerName("believer1")
+                        .birthOfYear("111111")
+                        .lunarSolarType(LunarSolarType.LUNAR)
+                        .address("부산")
+                        .build())
+                .lunarSolarType(LunarSolarType.LUNAR)
+                .birthOfYear("111111")
+                .etcValue("etc1")
+                .build();
+
+        fixtureDTOList = new ArrayList<>();
+        fixtureDTOList.add(fixtureDTO);
+    }
+
+    @Test
+    @DisplayName("가족 Entity에서 DTO로 변경 테스트")
+    void entityToDTO(){
+        //when
+        FamilyDTO result = familyMapper.entityToDTO(fixture);
+
+        //then
+        assertThat(result.getFamilyId(), is(1L));
+        assertThat(result.getFamilyName(), is("tester1"));
+        assertThat(result.getBirthOfYear(), is("111111"));
+        assertThat(result.getLunarSolarType(), is(LunarSolarType.LUNAR));
+        assertThat(result.getFamilyType(), is(FamilyType.FATHER));
+        assertThat(result.getEtcValue(), is("etc1"));
+        assertThat(result.getBeliever().getBelieverId(), is(1L));
+        assertThat(result.getBeliever().getBelieverName(), is("believer1"));
+        assertThat(result.getBeliever().getBirthOfYear(), is("111111"));
+        assertThat(result.getBeliever().getLunarSolarType(), is(LunarSolarType.LUNAR));
+        assertThat(result.getBeliever().getAddress(), is("부산"));
+    }
+
+    @Test
+    @DisplayName("가족 Entity에서 DTO로 변경 테스트 Entity Null")
+    void entityToDTONull(){
+        //when
+        FamilyDTO result = familyMapper.entityToDTO(null);
+
+        //then
+        assertThat(result, is(nullValue()));
+    }
+
+    @Test
+    @DisplayName("가족 Entity에서 DTO로 변경 테스트 Believer Null")
+    void entityToDTOBelieverNull(){
+        //given
+        Family fixture = Family.builder()
+                .familyId(1)
+                .familyName("tester1")
+                .familyType(FamilyType.FATHER)
+                .believer(null)
+                .lunarSolarType(LunarSolarType.LUNAR)
+                .birthOfYear("111111")
+                .etcValue("etc1")
+                .build();
+        //when
+        FamilyDTO result = familyMapper.entityToDTO(fixture);
+
+        //then
+        assertThat(result.getFamilyId(), is(1L));
+        assertThat(result.getFamilyName(), is("tester1"));
+        assertThat(result.getBirthOfYear(), is("111111"));
+        assertThat(result.getLunarSolarType(), is(LunarSolarType.LUNAR));
+        assertThat(result.getFamilyType(), is(FamilyType.FATHER));
+        assertThat(result.getEtcValue(), is("etc1"));
+    }
+
+    @Test
+    @DisplayName("가족 DTO에서 Entity로 변경 테스트")
+    void DTOToEntity(){
+        //when
+        Family result = familyMapper.DTOToEntity(fixtureDTO);
+
+        //then
+        assertThat(result.getFamilyId(), is(1L));
+        assertThat(result.getFamilyName(), is("tester1"));
+        assertThat(result.getBirthOfYear(), is("111111"));
+        assertThat(result.getLunarSolarType(), is(LunarSolarType.LUNAR));
+        assertThat(result.getFamilyType(), is(FamilyType.FATHER));
+        assertThat(result.getEtcValue(), is("etc1"));
+        assertThat(result.getBeliever().getBelieverId(), is(1L));
+        assertThat(result.getBeliever().getBelieverName(), is("believer1"));
+        assertThat(result.getBeliever().getBirthOfYear(), is("111111"));
+        assertThat(result.getBeliever().getLunarSolarType(), is(LunarSolarType.LUNAR));
+        assertThat(result.getBeliever().getAddress(), is("부산"));
+    }
+
+    @Test
+    @DisplayName("가족 DTO에서 Entity로 변경 테스트 Entity Null")
+    void DTOToEntityNull(){
+        //when
+        Family result = familyMapper.DTOToEntity(null);
+
+        //then
+        assertThat(result, is(nullValue()));
+    }
+
+    @Test
+    @DisplayName("가족 DTO에서 Entity로 변경 테스트 Believer Null")
+    void DTOToEntityBelieverNull(){
+        //given
+        FamilyDTO fixture = FamilyDTO.builder()
+                .familyId(1)
+                .familyName("tester1")
+                .familyType(FamilyType.FATHER)
+                .believer(null)
+                .lunarSolarType(LunarSolarType.LUNAR)
+                .birthOfYear("111111")
+                .etcValue("etc1")
+                .build();
+        //when
+        Family result = familyMapper.DTOToEntity(fixture);
+
+        //then
+        assertThat(result.getFamilyId(), is(1L));
+        assertThat(result.getFamilyName(), is("tester1"));
+        assertThat(result.getBirthOfYear(), is("111111"));
+        assertThat(result.getLunarSolarType(), is(LunarSolarType.LUNAR));
+        assertThat(result.getFamilyType(), is(FamilyType.FATHER));
+        assertThat(result.getEtcValue(), is("etc1"));
+    }
+
+    @Test
+    @DisplayName("가족 EntityList에서 DTOList로 변경 테스트")
+    void entityListToDTOList(){
+        //when
+        List<FamilyDTO> result = familyMapper.entityListToDTOList(fixtureList);
+
+        //then
+        assertThat(result.get(0).getFamilyId(), is(1L));
+        assertThat(result.get(0).getFamilyName(), is("tester1"));
+        assertThat(result.get(0).getBirthOfYear(), is("111111"));
+        assertThat(result.get(0).getLunarSolarType(), is(LunarSolarType.LUNAR));
+        assertThat(result.get(0).getFamilyType(), is(FamilyType.FATHER));
+        assertThat(result.get(0).getEtcValue(), is("etc1"));
+        assertThat(result.get(0).getBeliever().getBelieverId(), is(1L));
+        assertThat(result.get(0).getBeliever().getBelieverName(), is("believer1"));
+        assertThat(result.get(0).getBeliever().getBirthOfYear(), is("111111"));
+        assertThat(result.get(0).getBeliever().getLunarSolarType(), is(LunarSolarType.LUNAR));
+        assertThat(result.get(0).getBeliever().getAddress(), is("부산"));
+    }
+
+    @Test
+    @DisplayName("가족 EntityList에서 DTOList로 변경 테스트 Entity Null")
+    void entityListToDTOListNull(){
+        //when
+        List<FamilyDTO> result = familyMapper.entityListToDTOList(null);
+
+        //then
+        assertThat(result, is(nullValue()));
+    }
+}
