b68c8f1b8f7836db1baae355b92e53c85fce2391
ryukiyoun <ryukiyoun@naver.com>
2022-03-28 16:30:27 +0900

    feat: Date Util 추가
    - Date 관련 처리를 하는 Date Uil 추가
    
    - 공용 Template Date Util Import

---
+++ src/main/resources/static/js/util/date-util.js
@@ -0,0 +1,25 @@
+function getFormatDate(date){
+    var year = date.getFullYear();
+    var month = (1 + date.getMonth());
+    month = month >= 10 ? month : '0' + month;
+    var day = date.getDate();
+    day = day >= 10 ? day : '0' + day;
+    return  year + '' + month + '' + day;
+}
+
+function getDaysArray(year, month) {
+    let numDaysInMonth, daysInWeek, daysIndex, index, i, l, daysArray;
+
+    numDaysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    daysInWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    daysIndex = { 'Sun': 0, 'Mon': 1, 'Tue': 2, 'Wed': 3, 'Thu': 4, 'Fri': 5, 'Sat': 6 };
+    index = daysIndex[(new Date(year, month - 1, 1)).toString().split(' ')[0]];
+    daysArray = [];
+
+    for (i = 0, l = numDaysInMonth[month - 1]; i < l; i++) {
+        daysArray.push((i + 1) + ' 일');
+        if (index === 7) index = 0;
+    }
+
+    return daysArray;
+}
