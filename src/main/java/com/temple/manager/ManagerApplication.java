dc285514b5efc0c4ac464be7afbf3383e1cec1a6
ryukiyoun <ryukiyoun@naver.com>
2022-03-30 02:39:11 +0900

    feat: EnableJpaAuditing 제거
    - Application Class의 EnableJpaAuditing을 제거하고 Config로 분리
    
    - Test 수행 시 Config를 자동으로 등록을 위해 분리 작업 수행

--- src/main/java/com/temple/manager/ManagerApplication.java
+++ src/main/java/com/temple/manager/ManagerApplication.java
@@ -2,12 +2,10 @@ package com.temple.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @EnableRedisHttpSession
-@EnableJpaAuditing
 public class ManagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ManagerApplication.class, args);
