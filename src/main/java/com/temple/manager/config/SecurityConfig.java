02fe26dc63488904a2937eb660b0bec7bd618f33
ryukiyoun <ryukiyoun@naver.com>
2022-04-01 00:57:32 +0900

    feat: 신규 Login 화면 적용
    - 기존 Spring Security Default에서 신규 Login 화면으로 전환 적용

--- src/main/java/com/temple/manager/config/SecurityConfig.java
+++ src/main/java/com/temple/manager/config/SecurityConfig.java
@@ -5,10 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
@@ -41,32 +39,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/static/**");
-    }
-
-    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/static/**").permitAll()
                 .antMatchers("/profile").permitAll()
                 .antMatchers("/**").authenticated()
                 .and()
                 .formLogin()
                 .permitAll()
                 .and()
-                /*.loginPage("/login")
-                .loginProcessingUrl("/login/process")
-                .usernameParameter("username")
-                .passwordParameter("password")
-                .successHandler(new CustomLoginSuccessHandler(loggingRepository))
-                .permitAll()
-                .and()*/
+
                 //.addFilterAfter(new CustomAjaxSessionTimeoutFilter(), ExceptionTranslationFilter.class)
-                .csrf().ignoringAntMatchers("/h2-console/**")
+                .csrf()
                 .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 .and()
 
                 .headers().addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
+                .and()
+
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/login/process")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                //.successHandler(new CustomLoginSuccessHandler(loggingRepository))
+                .permitAll()
                 .and()
 
                 .logout()
