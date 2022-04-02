c1289c89a12c9d6bca17f36b954203deb4d78ef7
ryukiyoun <ryukiyoun@naver.com>
2022-04-02 15:44:30 +0900

    feat: Main DashBoard 화면 추가
    - Main DashBoard 화면 및 Controller 추가

---
+++ src/main/java/com/temple/manager/controller/DashBoardController.java
@@ -0,0 +1,15 @@
+package com.temple.manager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class DashBoardController {
+
+    @GetMapping("/")
+    public ModelAndView accessPage(ModelAndView mav){
+        mav.setViewName("page/dashboard");
+        return mav;
+    }
+}
