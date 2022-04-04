16bfd5ec8331d29f4f5095b70b97aa29160c3b67
ryukiyoun <ryukiyoun@naver.com>
2022-04-05 00:28:28 +0900

    feat: 기도 Aggregate 분리
    - Object로 참조하는 Believer와 Code를 long 타입의 Id만 참조하도록 Aggregate 분리 수행

--- src/main/java/com/temple/manager/prayer/entity/Prayer.java
+++ src/main/java/com/temple/manager/prayer/entity/Prayer.java
@@ -1,21 +1,26 @@
 package com.temple.manager.prayer.entity;
 
+import com.temple.manager.common.entity.BaseEntity;
 import com.temple.manager.prayer.dto.PrayerDTO;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Entity
 @Getter
-@Builder
+@SuperBuilder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "update prayer set active = date_format(now(), '%Y%m%d%H%k%s') where family_id = ?")
 @Where(clause = "active=99999999999999")
-public class Prayer {
+public class Prayer extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long prayerId;
@@ -29,21 +34,9 @@ public class Prayer {
     @JoinColumn(name = "PRAYER_TYPE_CODE_ID", nullable = false, referencedColumnName = "CODE_ID")
     private long prayerTypeCodeId;
 
-    @Column(nullable = false, length = 14)
-    String active;
-
-    @PrePersist
-    public void prePersist(){
-        this.active = "99999999999999";
-    }
-
     public void update(PrayerDTO prayerDTO){
         this.prayerStartDate = prayerDTO.getPrayerStartDate();
         this.believerId = prayerDTO.getBelieverId();
         this.prayerTypeCodeId = prayerDTO.getPrayerTypeCodeId();
-    }
-
-    public void delete(){
-        this.active = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
     }
 }
